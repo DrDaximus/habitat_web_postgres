38d3c098ffdcf607383871f49e45c374c59a80a3
Carl Goulding <carl_goulding@hotmail.com>
2016-12-15 23:00:21 +0000

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,29 @@
 
 ActiveRecord::Schema.define(version: 20161126131346) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
-    t.string   "reference"
-    t.string   "added_by",              default: "Unknown"
-    t.string   "job_type"
+    t.text     "reference"
+    t.text     "added_by",              default: "Unknown"
+    t.text     "job_type"
     t.integer  "stage",                 default: 1
     t.decimal  "quote"
     t.date     "start_date"
     t.integer  "team_id"
     t.boolean  "pif",                   default: false
     t.boolean  "contract_present",      default: false
-    t.string   "handled"
+    t.text     "handled"
     t.boolean  "q_sent",                default: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
-    t.string   "email"
+    t.text     "email"
     t.integer  "user_id"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "telephone"
-    t.string   "post_code"
+    t.text     "first_name"
+    t.text     "last_name"
+    t.text     "telephone"
+    t.text     "post_code"
     t.integer  "budget"
     t.integer  "when"
     t.text     "notes"
@@ -42,14 +45,14 @@ ActiveRecord::Schema.define(version: 20161126131346) do
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "contract_date"
-    t.string   "contract_file_name"
-    t.string   "contract_content_type"
+    t.text     "contract_file_name"
+    t.text     "contract_content_type"
     t.integer  "contract_file_size"
     t.datetime "contract_updated_at"
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string   "team_name"
+    t.text     "team_name"
     t.integer  "team_no"
     t.integer  "user_id"
     t.datetime "created_at", null: false
@@ -57,11 +60,11 @@ ActiveRecord::Schema.define(version: 20161126131346) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "lastname"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "reference"
+    t.text     "name"
+    t.text     "lastname"
+    t.text     "email"
+    t.text     "password_digest"
+    t.text     "reference"
     t.integer  "role"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
